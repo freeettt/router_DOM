344e8763fc342c678385f524c63b7c6f496d02af
freeettt <122921308+freeettt@users.noreply.github.com>
2025-05-06 20:59:52 +0300

    Create HomePage.js

---
+++ router/src/pages /HomePage.js
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    <section className="section">
+      <div className="container has-text-centered">
+        <h1 className="title is-2">Головна сторінка</h1>
+        <p className="subtitle is-4">Ласкаво просимо до нашого сайту!</p>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
