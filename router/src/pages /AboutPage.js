ed45d805505a7cef1f4b15d112866e814f04859b
freeettt <122921308+freeettt@users.noreply.github.com>
2025-05-06 20:59:15 +0300

    Create AboutPage.js

---
+++ router/src/pages /AboutPage.js
@@ -0,0 +1,14 @@
+const AboutPage = () => {
+  return (
+    <section className="section">
+      <div className="container content">
+        <h1 className="title is-2">Про нас</h1>
+        <p className="is-size-5">
+          Ми — команда, яка займається розробкою сучасних веб-додатків. Наша мета — створення якісних продуктів.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutPage;
