e4cd909f44e86c6f49dea8d308e3bae43bd9eb8f
freeettt <122921308+freeettt@users.noreply.github.com>
2025-05-06 21:00:07 +0300

    Create NotFoundPage.js

---
+++ router/src/pages /NotFoundPage.js
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+  return (
+    <section className="section">
+      <div className="container has-text-centered">
+        <h1 className="title is-1 has-text-danger">404</h1>
+        <p className="subtitle is-4">Сторінку не знайдено</p>
+      </div>
+    </section>
+  );
+};
+
+export default NotFoundPage;
